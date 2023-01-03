4c590d4fbb909fcbf78afded5cdd44339c366031
piyush <sainipiyus11111@gmail.com>
2023-01-03 09:27:13 +0530

    amazing code

---
+++ piyu.js
@@ -0,0 +1,9 @@
+var title= document.querySelector('.list-group-item')
+title.style.color='blue'
+var title= document.querySelector('input')
+title.value='hell'
+var odd= document.querySelectorAll('li:nth-child(odd)')
+for(i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor='#CCC'
+}
